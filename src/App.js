9760d7976f41a9393af951bc7d5709af3ec81d4a
monika1410 <monika.skowronska1410@gmail.com>
2024-06-16 19:24:29 +0200

    Update App.js to fix API request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import RecipeList from './components/RecipeList';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    const handleSearch = async (query) => {
+        const response = await axios.get('https://api.edamam.com/api/recipes/v2', {
+            params: {
+                q: query,
+                app_id: '99d15ec1',
+                app_key: '81a8c22bcb6cc758d922c18dcd8fee90',
+                type: 'public'
+            }
+        });
+        setRecipes(response.data.hits);
+    };
+
+    return (
+        <div>
+            <Header />
+            <SearchBar onSearch={handleSearch} />
+            <RecipeList recipes={recipes} />
+        </div>
+    );
+};
 
 export default App;
